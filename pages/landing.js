1299cc123894fa189f6651823842206ca6e489ce
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2025-02-25 08:08:26 +0530

    created

---
+++ pages/landing.js
@@ -0,0 +1,50 @@
+import LatestOffers from "@/components/LatestOffers/";
+import Header from "@/components/Header";
+import LandingBanner from "@/components/LandingBanner";
+import BrandsSlider from "@/components/BrandsSlider";
+import AppleProducts from "@/components/AppleProducts";
+import Mobile from "@/components/Mobile";
+import Computing from "@/components/Computing";
+import Camera from "@/components/Camera";
+import Accessories from "@/components/Accessories";
+import IconBar from "@/components/IconBar";
+import Footer from "@/components/Footer";
+import Image from "next/image";
+
+
+export default function LandingPage() {
+  return (
+    <>
+      <Header />
+      <div className="relative">
+        <Image
+          src="/images/bgLineWide.jpg"
+          alt="line"
+          width={100}
+          height={30}
+          className="absolute top-0 right-0 w-fit h-[670px] object-cover z-0 opacity-5 items-end"
+        />
+
+        <Image
+          src="/images/bgLineThin.jpg"
+          alt="line"
+          width={200}
+          height={500}
+          className="absolute top-0 right-40 w-fit h-[670px] object-cover z-0 opacity-5 items-end"
+        />
+      </div>
+
+      <LandingBanner />
+      <LatestOffers/>
+      <BrandsSlider/>
+      <AppleProducts/>
+      <Mobile/>
+      <Computing/>
+      <Camera/>
+      <Accessories/>
+      <IconBar/>
+      <Footer/>
+
+    </>
+  );
+}
