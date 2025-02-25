32c71f2c01d216deb5daccaf282fa601bfbe12de
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2025-02-25 07:50:27 +0530

    finished desktop view

--- components/LandingBanner.js
+++ components/LandingBanner.js
@@ -14,95 +14,175 @@ const LandingBanner = () => {
           priority
         />
 
-  {/* Product Names */}
-<div 
-  className="absolute z-20 flex flex-col items-start" 
-  style={{ top: "10%", left: "12%" }}
->
-  <div 
-    style={{ 
-      width: "164px",
-      height: "19px",
-      fontFamily: "Kanit", 
-      fontWeight: 275, 
-      fontSize: "16px", 
-      lineHeight: "19.2px", 
-      letterSpacing: "10%",
-      color: "#D9D9D9" ,// Less bright white
-      marginBottom: "10px"
-    }}
-  >
-    OSMO POCKET 3
-  </div>
-  <div 
-    style={{
-      width: "164px",
-      height: "19px",
-      fontFamily: "Kanit", 
-      fontWeight: 700, 
-      fontSize: "16px", 
-      lineHeight: "19.2px", 
-      letterSpacing: "10%",
-      color: "#FEFEFF" ,
-      marginBottom: "10px"
-    }}
-  >
-    DJI RS 4 PRO
-  </div>
-  <div 
-    style={{ 
-      fontFamily: "Kanit", 
-      fontWeight: 275, 
-      fontSize: "16px", 
-      lineHeight: "19.2px", 
-      letterSpacing: "10%",
-      color: "#D9D9D9" // Less bright white
-    }}
-  >
-    IPHONE 15 PRO
-  </div>
+        {/* Product Names */}
+        <div className="absolute z-20 flex flex-col items-start" style={{ top: "10%", left: "8%" }}>
+          <div
+            style={{
+              width: "164px",
+              height: "19px",
+              fontFamily: "Kanit",
+              fontWeight: 275,
+              fontSize: "16px",
+              lineHeight: "19.2px",
+              letterSpacing: "10%",
+              color: "#D9D9D9", // Less bright white
+              marginBottom: "10px",
+            }}
+          >
+            OSMO POCKET 3
+          </div>
+          <div
+            style={{
+              width: "164px",
+              height: "19px",
+              fontFamily: "Kanit",
+              fontWeight: 700,
+              fontSize: "16px",
+              lineHeight: "19.2px",
+              letterSpacing: "10%",
+              color: "#FEFEFF",
+              marginBottom: "10px",
+            }}
+          >
+            DJI RS 4 PRO
+          </div>
+          <div
+            style={{
+              fontFamily: "Kanit",
+              fontWeight: 275,
+              fontSize: "16px",
+              lineHeight: "19.2px",
+              letterSpacing: "10%",
+              color: "#D9D9D9", // Less bright white
+            }}
+          >
+            IPHONE 15 PRO
+          </div>
+        </div>
+
+        {/* Slide Navigator (Vertical Bar) */}
+        <div
+          className="absolute z-20"
+          style={{
+            top: "15%",
+            left: "6%",
+            width: "22px",
+            height: "2px",
+            backgroundColor: "#FEFEFF",
+            transform: "rotate(-90deg)",
+            transformOrigin: "left top",
+          }}
+        ></div>
+
+{/* Left and Right Arrows */}
+<div className="absolute top-1/2 left-0 right-0 flex justify-between items-center px-8 z-20">
+  {/* Left Arrow */}
+  <button className="bg-transparent p-2 flex items-center">
+    <Image src="/images/left-arrow.png" alt="Previous" width={20} height={20} className="h-8 w-auto" />
+  </button>
+
+  {/* Right Arrow */}
+  <button className="bg-transparent p-2 flex items-center">
+    <Image src="/images/right-arrow.png" alt="Next" width={20} height={20} className="h-8 w-auto" />
+  </button>
 </div>
 
-{/* Slide Navigator (Vertical Bar) */}
-<div 
-  className="absolute z-20" 
-  style={{ 
-    top: "15%",
-    left: "11%", 
-    width: "22px", 
-    height: "2px", 
-    backgroundColor: "#FEFEFF", 
-    transform: "rotate(-90deg)", 
-    transformOrigin: "left top" 
-  }}
-></div>
 
         {/* "NEW" text */}
-        <div className="absolute z-20 text-white" style={{ top: "35%", left: "12%", fontFamily: "Poppins", fontWeight: 400, fontSize: "20px", lineHeight: "24px" }}>
+        <div className="absolute z-20 text-[#FEFEFF]" 
+        style={{ 
+          top: "55%", 
+          left: "6%", 
+          fontFamily: "Kanit",
+          fontWeight: 400,
+          fontSize: "23px", 
+          lineHeight: "27.6px" 
+            }}
+            >
           NEW
         </div>
 
         {/* Main Heading */}
-        <div className="absolute z-20 text-white" style={{ top: "40%", left: "12%" }}>
-          <h1 style={{ fontFamily: "Poppins", fontWeight: 500, fontSize: "75.07px", lineHeight: "87.63px", margin: 0 }}>
+        <div className="absolute z-20 text-[#FEFEFF]" 
+        style={{ 
+          top: "58%",
+          left: "6.1%" 
+          }}
+          >
+          <h1 style={{ 
+            fontFamily: "Kanit",
+            fontWeight: 400, 
+            fontSize: "64px",
+            lineHeight: "76.8px",
+            margin: 0 
+            }}
+               >
             RONIN RS 4 PRO
           </h1>
         </div>
 
         {/* Subheading */}
-        <div className="absolute z-20 text-white" style={{ top: "60%", left: "12%" }}>
-          <p style={{ fontFamily: "Poppins", fontWeight: 400, fontSize: "29.14px", lineHeight: "33.99px", margin: 0 }}>
-            TRANSCEND POTENTIAL <br />
-            In publishing and graphic design. Lorem ipsum is placeholder text commonly used to demonstrate <br />
-            the visual form of a document or a typeface without relying on meaningful content. Lorem <br />
-            may be used as a placeholder before the final copy is available.
-          </p>
-        </div>
+<div className="absolute z-20 text-white" 
+  style={{ 
+    top: "67%", 
+    left: "6.1%" 
+  }}
+>
+  <p style={{ 
+    fontFamily: "Kanit", 
+    fontWeight: "400", 
+    fontSize: "23px", 
+    lineHeight: "27.6px", 
+    margin: 0,
+    marginTop:"20px"
+  }}>
+    TRANSCEND POTENTIAL 
+  </p>
+  <br/>
+  <p 
+  style={{
+ 
+  fontFamily: "Kanit",
+  fontWeight: "275",
+  fontSize: "14px",
+  lineHeight: "16.8px",
+  margin: 0,
+  color: "#D9D9D9" // Less bright white (light gray)
+}}>
+    In publishing and graphic design. Lorem ipsum is placeholder text commonly used to demonstrate <br />
+    the visual form of a document or a typeface without relying on meaningful content. Lorem <br />
+    may be used as a placeholder before the final copy is available.
+  </p>
+</div>
 
         {/* Buttons */}
-        <div className="absolute z-20 flex gap-4" style={{ bottom: "10%", left: "12%" }}>
-          <button className="bg-transparent border border-white text-white px-8 py-3 rounded-full" style={{ fontFamily: "Poppins", fontWeight: 500, fontSize: "20px", lineHeight: "24px" }}>BUY NOW</button>
-          <button className="bg-transparent border border-white text-white px-8 py-3 rounded-full" style={{ fontFamily: "Poppins", fontWeight: 500, fontSize: "20px", lineHeight: "24px" }}>LEARN MORE</button>
+        <div className="absolute z-20 flex gap-4" 
+        style={{ 
+          bottom: "13%", 
+          left: "6.1%",
+          
+          }}
+          >
+          <button className="bg-transparent border border-white text-white px-8 py-3 " 
+          style={{ 
+            fontFamily: "Kanit", 
+            fontWeight: 400, 
+            fontSize: "20px", 
+            lineHeight: "24px" 
+            }}
+            >
+              BUY NOW
+            </button>
+          <button className="bg-transparent border border-white text-white px-8 py-3 " 
+          style={{ 
+            fontFamily: "Kanit", 
+            fontWeight: 400, 
+            fontSize: "20px", 
+            lineHeight: "24px" 
+            }}
+            >
+            LEARN MORE
+            </button>
         </div>
       </div>
 
@@ -112,7 +192,7 @@ const LandingBanner = () => {
         <div
           className="w-full h-full"
           style={{
-            backgroundImage: "url('/images/dell-unsplash 1.png')",
+            backgroundImage: "url('/images/LandingBanner.png')",
             backgroundSize: "cover",
             backgroundPosition: "center",
             zIndex: "-1",
