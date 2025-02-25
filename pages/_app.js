ef6c9a78726d7a92e62a55ae2fdd583f58eb4fa5
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2025-02-25 08:07:19 +0530

    created

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import '../styles/globals.css';  // Ensure the correct path to your global styles
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+export default MyApp;
